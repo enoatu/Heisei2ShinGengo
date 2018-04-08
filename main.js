6cd00dfd38c3369baea9341c51060e0a7cf344b1
enoatu <acro1452@gmail.com>
2018-04-08 19:08:08 +0900

    安始の場合2

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-document.body.innerHTML = document.body.innerHTML.split('平成to新元号').join('新元号（作成時は不明）');
+document.body.innerHTML = document.body.innerHTML
+    .split('平成to新元号')
+    //.join('新元号（作成時は不明）');
+    .join('安始');
+
